ec712410e320dd0bf928b8d64d309a09ffe2ae54
Clayton <clayton_monson@hotmail.com>
2022-10-12 18:16:41 -0300

    Service layer, component registration

---
+++ src/main/java/com/claytoncorp/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.claytoncorp.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.claytoncorp.course.entities.User;
+import com.claytoncorp.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
