473e937a5de6f02a0fd5281dfed2b329a9b4cd06
Clayton <clayton_monson@hotmail.com>
2022-11-02 18:29:07 -0300

    Exception handling - findById

--- src/main/java/com/claytoncorp/course/services/UserService.java
+++ src/main/java/com/claytoncorp/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.claytoncorp.course.entities.User;
 import com.claytoncorp.course.repositories.UserRepository;
+import com.claytoncorp.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -43,5 +44,5 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-	
+
 }
